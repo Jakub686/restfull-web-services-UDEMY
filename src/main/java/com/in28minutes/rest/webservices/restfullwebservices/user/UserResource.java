4d92889d22ab00d5163dc3d9e63e92a39011f0da
Jakub <jakub.bchyla@gmail.com>
2021-07-04 20:12:29 +0200

    Step: 70

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserResource.java
@@ -1,19 +1,25 @@
 package com.in28minutes.rest.webservices.restfullwebservices.user;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.hateoas.Resource;
-//import org.springframework.hateoas.mvc.ControllerLinkBuilder;
-//import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import javax.validation.Valid;
-import java.net.URI;
-import java.nio.file.attribute.UserPrincipalNotFoundException;
-import java.util.List;
-import java.util.ResourceBundle;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -21,37 +27,52 @@ public class UserResource {
     @Autowired
     private UserDaoService service;
 
-    @GetMapping(
-            "/users")
-    public List<User> retrieveAllUsers(){
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
         return service.findAll();
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
+
         if(user==null)
-            throw new UserNotFoundException("id-" + id);
+            throw new UserNotFoundException("id-"+ id);
 
-//        //HATEOAS
-//        Resource<User> resource = new Resource<User>(user);
-//        ControllerLinkBuilder linkTo =  linkTo(methodOn(this.getClass()).retrieveAllUsers());
-//        resource.add(linkTo.withRel("all-users"));
 
-        return user;
-        //return resource;
+        //"all-users", SERVER_PATH + "/users"
+        //retrieveAllUsers
+        EntityModel<User> resource = EntityModel.of(user);
+
+        WebMvcLinkBuilder linkTo =
+                linkTo(methodOn(this.getClass()).retrieveAllUsers());
+
+        resource.add(linkTo.withRel("all-users"));
+
+        //HATEOAS
+
+        return resource;
     }
 
     @DeleteMapping("/users/{id}")
-    public void deleteUser(@PathVariable int id){
+    public void deleteUser(@PathVariable int id) {
         User user = service.deleteById(id);
+
         if(user==null)
-            throw new UserNotFoundException("id-" + id);
+            throw new UserNotFoundException("id-"+ id);
     }
 
+    //
+    // input - details of user
+    // output - CREATED & Return the created URI
+
+    //HATEOAS
+
     @PostMapping("/users")
-    public ResponseEntity<Object> createUser(@Valid @RequestBody User user){
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
+        // CREATED
+        // /user/{id}     savedUser.getId()
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -59,5 +80,6 @@ public class UserResource {
                 .buildAndExpand(savedUser.getId()).toUri();
 
         return ResponseEntity.created(location).build();
+
     }
 }
