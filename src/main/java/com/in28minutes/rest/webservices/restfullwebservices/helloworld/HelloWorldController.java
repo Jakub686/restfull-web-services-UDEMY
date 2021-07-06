c2bcccdc3ddc6b5782c3abeffe339c3665374ef6
Jakub <jakub.bchyla@gmail.com>
2021-07-06 19:31:25 +0200

    Step: 70

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,18 @@
 package com.in28minutes.rest.webservices.restfullwebservices.helloworld;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 //Controller
 @RestController
 public class HelloWorldController {
 
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/hello-World")
     public String helloWorld() {
@@ -21,6 +27,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World,%s",name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name="Accept-language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message",null,null,locale);
     }
 
 }
