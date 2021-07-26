3b9dccaf4fb01b45a82c67b2b726fe6bd7f16e96
Jakub <jakub.bchyla@gmail.com>
2021-07-26 19:36:47 +0200

    last working

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/User.java
@@ -1,13 +1,21 @@
 package com.in28minutes.rest.webservices.restfullwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Size;
 
 
-
+@ApiModel(description = "All details about the user. ")
+@Entity
 public class User {
 
-
+    @Id
+    @GeneratedValue
     private Integer id;
+
     @Size(min=2, message ="Name should have at least 2 char")
     private String name;
         //private Data birthDate;
---
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user VALUES(1, 'ABc');
+insert into user values(2, 'Jill');
+insert into user values(3, 'Jam');
